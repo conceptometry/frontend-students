63568fd6564f1a0d1c5f68f06f99aa82cadb7332
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-03-08 12:45:19 +0530

    Added msft clarity to pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -67,6 +67,21 @@ function MyApp({ Component, pageProps, router }: Props) {
       />
     );
   }
+
+  useEffect(() => {
+    (function (c, l, a, r, i, t, y) {
+      c[a] =
+        c[a] ||
+        function () {
+          (c[a].q = c[a].q || []).push(arguments);
+        };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = 'https://www.clarity.ms/tag/' + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, 'clarity', 'script', '5q72romt2t');
+  }, []);
   return (
     <CookiesProvider>
       <StateProvider reducer={reducer} initialState={initialState}>
